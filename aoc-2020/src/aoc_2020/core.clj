fcb1b88366a5f0e7a45b6525b6c8e859b734df5c
Arren Mund <amund@agleader.com>
2020-12-06 00:17:23 -0600

    updated core for day6

--- aoc-2020/src/aoc_2020/core.clj
+++ aoc-2020/src/aoc_2020/core.clj
@@ -4,27 +4,38 @@
   (:require [aoc-2020.day-three :as day3])
   (:require [aoc-2020.day-four :as day4])
   (:require [aoc-2020.day-five :as day5])
+  (:require [aoc-2020.day-six :as day6])
   (:gen-class))
 
 (defn -main []
   (println "==============" "AOC2020 Day 1" "==============")
   (time (day1/aoc-day1-p1))
   (time (day1/aoc-day1-p2))
+  (println)
 
   (println "==============" "AOC2020 Day 2" "==============")
   (time (day2/aoc-day2-p1-reduce))
   (time (day2/aoc-day2-p2-reduce))
+  (println)
 
   (println "==============" "AOC2020 Day 3" "==============")
   (time (day3/aoc-day3-p1))
   (time (day3/aoc-day3-p2))
+  (println)
 
   (println "==============" "AOC2020 Day 4" "==============")
   (time (day4/aoc-day4-p1))
   (time (day4/aoc-day4-p2))
+  (println)
 
   (println "==============" "AOC2020 Day 5" "==============")
   (time (day5/aoc-day5-p1))
-  (time (day5/aoc-day5-p2)))
+  (time (day5/aoc-day5-p2))
+  (println)
+  
+  (println "==============" "AOC2020 Day 6" "==============")
+  (time (day6/aoc-day6-p1))
+  (time (day6/aoc-day6-p2))
+  (println))
 
 
